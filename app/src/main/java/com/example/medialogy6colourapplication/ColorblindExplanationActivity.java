17587e225b47bbc795669cfe804e1abfa2308b50
TobiasRisom <trisom19@student.aau.dk>
2023-05-09 12:10:32 +0200

    new activities

---
+++ app/src/main/java/com/example/medialogy6colourapplication/ColorblindExplanationActivity.java
@@ -0,0 +1,14 @@
+package com.example.medialogy6colourapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorblindExplanationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colorblind_explanation);
+    }
+}
